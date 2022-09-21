6e7549a27267f119a47c32aaada60f939e1678a2
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-09-20 23:03:26 -0300

    fazendo o ordenador e filtro

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,10 +2,12 @@ import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
 import Buscador from './Buscador';
 import { useState } from 'react';
+import Filtros from './Filtros';
 
 export default function Cardapio() {
 
     const [busca, setBusca] = useState('');
+    const [filtro, setFiltro] = useState<number | null>(null);
 
     return (
         <main>
@@ -18,6 +20,9 @@ export default function Cardapio() {
             <section className={styles.cardapio}>
                 <h3 className={styles.cardapio__titulo}>Cardápio</h3>
                 <Buscador busca={busca} setBusca={setBusca} />
+                <div className={styles.cardapio__filtros}>
+                    <Filtros filtro={filtro} setFiltro={setFiltro} />
+                </div>
             </section>
         </main>
     )
