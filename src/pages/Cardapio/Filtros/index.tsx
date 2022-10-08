bfc640c9b2e2a8e61290cbae3c4ee1ff64638411
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-10-07 22:14:01 -0300

    melhorando o prjeto com novo curso

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -6,29 +6,29 @@ import classNames from 'classnames';
 type IOpcao = typeof filtros[0];
 
 interface Props {
-    filtro: number | null;
-    setFiltro: React.Dispatch<React.SetStateAction<number | null>>
+  filtro: number | null;
+  setFiltro: React.Dispatch<React.SetStateAction<number | null>>
 }
 
 export default function Filtros({ filtro, setFiltro }: Props) {
 
-    function selecionarFiltro(opcao: IOpcao) {
-        if (filtro === opcao.id) return setFiltro(null);
-        return setFiltro(opcao.id);
-    }
+  function selecionarFiltro(opcao: IOpcao) {
+    if (filtro === opcao.id) return setFiltro(null);
+    return setFiltro(opcao.id);
+  }
 
-    return (
-        <div className={styles.filtros}>
-            {filtros.map(opcao => (
-                <button
-                    className={classNames({
-                        [styles.filtros__filtro]: true,
-                        [styles['filtros__filtro--ativo']]: filtro === opcao.id,
-                    })}
-                    key={opcao.id} onClick={() => selecionarFiltro(opcao)}>
-                    {opcao.label}
-                </button>
-            ))}
-        </div>
-    );
+  return (
+    <div className={styles.filtros}>
+      {filtros.map(opcao => (
+        <button
+          className={classNames({
+            [styles.filtros__filtro]: true,
+            [styles['filtros__filtro--ativo']]: filtro === opcao.id,
+          })}
+          key={opcao.id} onClick={() => selecionarFiltro(opcao)}>
+          {opcao.label}
+        </button>
+      ))}
+    </div>
+  );
 }
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -8,27 +8,27 @@ import Itens from './Itens';
 
 export default function Cardapio() {
 
-    const [busca, setBusca] = useState('');
-    const [filtro, setFiltro] = useState<number | null>(null);
-    const [ordenador, setOrdernador] = useState("");
+  const [busca, setBusca] = useState('');
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdernador] = useState('');
 
-    return (
-        <main>
-            <nav className={styles.menu}>
-                <Logo />
-            </nav>
-            <header className={styles.header}>
-                <div className={styles.header__text}>A casa do código e da massa</div>
-            </header>
-            <section className={styles.cardapio}>
-                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-                <Buscador busca={busca} setBusca={setBusca} />
-                <div className={styles.cardapio__filtros}>
-                    <Filtros filtro={filtro} setFiltro={setFiltro} />
-                    <Ordenador ordenador={ordenador} setOrdenador={setOrdernador} />
-                </div>
-                <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
-            </section>
-        </main>
-    )
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>A casa do código e da massa</div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filtros}>
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdernador} />
+        </div>
+        <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+      </section>
+    </main>
+  );
 }
